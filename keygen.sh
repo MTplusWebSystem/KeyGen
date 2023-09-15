0c414ad2099507b6ef1eeb20ae2d819c11422f10
MTplusWebSystem <128556678+MTplusWebSystem@users.noreply.github.com>
2023-09-14 21:19:02 -0400

    Update keygen.sh

--- keygen.sh
+++ keygen.sh
@@ -1,41 +1,6 @@
 #!/bin/bash
 
 
-echo "
-      █ █▄░█ █▀ ▀█▀ ▄▀█ █░░ █░░
-      █ █░▀█ ▄█ ░█░ █▀█ █▄▄ █▄▄"
-      
-
-lista="openjdk-17 openssl openssl-tool"
-
-
-if [ ! -f controle_instalacao ]; then
-    apt update -y
-    apt upgrade -y
-    touch controle_instalacao
-    touch limite.txt
-fi
-cp keygen.sh /data/data/com.termux/files/usr/bin/keygen 
-chmod  777 /data/data/com.termux/files/usr/bin/keygen 
-rm ~/keygen.sh
-echo -n "["
-for dependencia in $lista
-do
-    # Verifique se a dependência já está instalada antes de tentar instalá-la novamente
-    if ! dpkg -l | grep -q $dependencia; then
-        pkg install $dependencia -y >> /dev/null
-    fi
-    
-    dependencias_instaladas=$((dependencias_instaladas + 1))
-    progresso=$((dependencias_instaladas * 100 / total_dependencias))
-    barra_de_progresso=$(printf '# %.0s' $(seq 1 $((progresso / 10))))
-    echo -ne "$barra_de_progresso] $progresso%\r"
-    
-done
- 
- echo "1" > limite.txt
- 
-echo -e "\n\nTodas as dependências foram instaladas com sucesso."
 
 clear
 #menu inicio
