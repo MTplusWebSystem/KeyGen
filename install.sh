97c5b37965dd96d99df512742653fd1b8f87879e
MTplusWebSystem <128556678+MTplusWebSystem@users.noreply.github.com>
2023-09-14 21:18:09 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,19 @@ wget https://github.com/MTplusWebSystem/KeyGen/blob/main/keygen.sh;
 chmod 777 keygen.sh;
 
 ./keygen.sh
+
+
+
+for dependencia in $lista
+do
+    # Verifique se a dependência já está instalada antes de tentar instalá-la novamente
+    if ! dpkg -l | grep -q $dependencia; then
+        pkg install $dependencia -y >> /dev/null
+    fi
+    
+    dependencias_instaladas=$((dependencias_instaladas + 1))
+    progresso=$((dependencias_instaladas * 100 / total_dependencias))
+    barra_de_progresso=$(printf '# %.0s' $(seq 1 $((progresso / 10))))
+    echo -ne "$barra_de_progresso] $progresso%\r"
+    
+done
